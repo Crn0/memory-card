6f265fa253b2f17b4b909465606acd08be509015
EssenceCode <123456789+odin@users.noreply.github.com>
2024-02-26 14:52:25 +0800

    Fix alignment

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="text-center pad1rem hex-820347 margin-top">
+        <header className="grid place-center-center margin-bottom text-center pad1rem hex-820347">
             <h1 className="font-white">Memory Cards</h1>
         </header>
     )
--- src/component/win-dialog/Dialog.jsx
+++ src/component/win-dialog/Dialog.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable react/prop-types */
 export default function Dialog({score, handleWin}) {
     return (
-        <div className='grid place-center-center pad1rem font-white hex-820347'>
+        <div className='grid place-center-center margin-auto pad1rem font-white hex-820347'>
           <h1>YOU WIN</h1>
-          <p className='.font2rem bold'>Score: {score.best}</p>
-          <button onClick={() => handleWin()}>Restart</button>
+          <p className='font2rem bold'>Score: {score.best}</p>
+          <button className="pad1rem" onClick={() => handleWin()}>Restart</button>
         </div>
     )
 }
