95ca7bf36bbd8a736ab9b55a058e8d9fee0f761b
EssenceCode <123456789+odin@users.noreply.github.com>
2024-02-26 13:58:37 +0800

    Create footer component

---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import { FaGithub } from "react-icons/fa";
+export default function Footer() {
+    return (
+        <footer className="flex gap hex-820347 pad1rem">
+            <p>
+                <a 
+                    href="https://github.com/EssenceCode" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
+                    EssenceCode
+                    {" "}
+                    <FaGithub/>
+                </a>
+        </p>
+        </footer>
+    )
+}
