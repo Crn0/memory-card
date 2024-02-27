0222f0fea6c41a42b59add13eeb1013b351536da
EssenceCode <123456789+odin@users.noreply.github.com>
2024-02-27 13:56:37 +0800

    Add home button

--- src/component/win-dialog/Dialog.jsx
+++ src/component/win-dialog/Dialog.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable react/prop-types */
-export default function Dialog({score, handleWin}) {
+export default function Dialog({score, handleWin,handleHome}) {
     return (
         <div className='grid place-center-center margin-auto pad1rem font-white hex-820347'>
           <h1>YOU WIN</h1>
           <p className='font2rem bold'>Score: {score.best}</p>
-          <button className="pad1rem" onClick={() => handleWin()}>Restart</button>
+          <div className="flex gap">
+            <button className="pad1rem btn-hover" onClick={() => handleWin()}>Restart</button>
+            <button className="pad1rem btn-hover" onClick={() => handleHome()}>Home</button>
+          </div>
         </div>
     )
 }
