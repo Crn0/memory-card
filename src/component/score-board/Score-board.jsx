bd4e794aaa25f6bd66bbd081408a885dd323b4bd
EssenceCode <123456789+odin@users.noreply.github.com>
2024-02-27 14:11:16 +0800

    Remove class

--- src/component/score-board/Score-board.jsx
+++ src/component/score-board/Score-board.jsx
@@ -2,7 +2,7 @@
 export default function ScoreBoard({score, pokemonList}) {
     return (
         <div className="flex align-items-center space-around">
-            <p className="font2rem bold btn-hover">Score: {score.current}/{pokemonList.length}</p>
+            <p className="font2rem bold">Score: {score.current}/{pokemonList.length}</p>
             <p className="font2rem bold">Best Score: {score.best}</p>
         </div>
     )
