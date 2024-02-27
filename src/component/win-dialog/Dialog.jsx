4fd4e8fdf05c76c36d90e4ef0ba2ae149cb8c0d0
EssenceCode <123456789+odin@users.noreply.github.com>
2024-02-27 14:09:30 +0800

    Change button to restart

--- src/component/win-dialog/Dialog.jsx
+++ src/component/win-dialog/Dialog.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
-export default function Dialog({score, handleWin,handleHome}) {
+export default function Dialog({score, handleWin,handleRestart}) {
     return (
         <div className='grid place-center-center margin-auto pad1rem font-white hex-820347'>
           <h1>YOU WIN</h1>
           <p className='font2rem bold'>Score: {score.best}</p>
           <div className="flex gap">
-            <button className="pad1rem btn-hover" onClick={() => handleWin()}>Restart</button>
-            <button className="pad1rem btn-hover" onClick={() => handleHome()}>Home</button>
+            <button className="pad1rem btn-hover" onClick={() => handleWin()}>Play Again</button>
+            <button className="pad1rem btn-hover" onClick={() => handleRestart()}>Restart</button>
           </div>
         </div>
     )
