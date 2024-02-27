a4191f5ebfa6485a7b8184a6d701947a5593498a
EssenceCode <123456789+odin@users.noreply.github.com>
2024-02-27 13:58:09 +0800

    Decouple css to different class

--- src/component/score-board/Score-board.jsx
+++ src/component/score-board/Score-board.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 export default function ScoreBoard({score, pokemonList}) {
     return (
-        <div className="flex">
-            <p className="font2rem bold">Score: {score.current}/{pokemonList.length}</p>
+        <div className="flex align-items-center space-around">
+            <p className="font2rem bold btn-hover">Score: {score.current}/{pokemonList.length}</p>
             <p className="font2rem bold">Best Score: {score.best}</p>
         </div>
     )
